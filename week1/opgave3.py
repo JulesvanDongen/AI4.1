fdf023ad2ee23969755da0cb4b24ac6db4708453
Jules van Dongen <julesvandongen@gmail.com>
2018-10-10 14:26:59 +0200

    Almost finished recursive network, just need to fix bugs

--- week1/opgave3.py
+++ week1/opgave3.py
@@ -13,15 +13,63 @@ board = [
 currentPos = [0, 4]
 currentValue = 1
 
-def move(currentPos, board):
+def isFinished(board):
+    for y in board:
+        for x in y:
+            if x != -1:
+                return False
+    return True
+
+def getSurroundingPositions(position, board, stepcount):
+    # TODO: implement step count
+
+    returnedPositions = []
+    if position[0] > 0:
+        appendedPosition = [position[0] - 1, position[1]]
+        if board[appendedPosition[0]][appendedPosition[1]] != -1:
+            returnedPositions.append(appendedPosition)
+    if position[0] < len(board) - 1:
+        appendedPosition = [position[0] + 1, position[1]]
+        if board[appendedPosition[0]][appendedPosition[1]] != -1:
+            returnedPositions.append(appendedPosition)
+    if position[1] > 0:
+        appendedPosition = [position[0], position[1] - 1]
+        if board[appendedPosition[0]][appendedPosition[1]] != -1:
+            returnedPositions.append(appendedPosition)
+    if position[1] < len(board[0]) - 1:
+        appendedPosition = [position[0], position[1] + 1]
+        if board[appendedPosition[0]][appendedPosition[1]] != -1:
+            returnedPositions.append(appendedPosition)
+
+    return returnedPositions
+
+def move(moves, board):
+    # Remove the last position from the board
+    lastMove = moves[len(moves)-1]
+    board[lastMove[0]][lastMove[1]] = -1
+
+    if isFinished(board):
+        return moves
+    else:
+        # Find the possible moves, if there are no possible moves left which can be used, return Null
+        # print(getSurroundingPositions(lastMove, board))
+        for pos in getSurroundingPositions(lastMove, board, len(moves) + 1):
+            if pos == [8,0]:
+                print(board)
+
+            newboard = board
+            newMoves = moves
+            newMoves.append(pos)
+            result = move(newMoves, newboard)
+            if result is not None:
+                return result
+        return None
     pass
 
-def getSurroundingPositions(position, board):
-    if position[0] == 0:
+allMoves = move([currentPos], board)
 
-    pass
-
-print(board)
-print(reduceBoardValues(board))
+print(allMoves)
+# print(board)
+# print(getSurroundingPositions([1,1], board))
 
 
